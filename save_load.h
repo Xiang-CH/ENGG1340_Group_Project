7f3d818d72060d6cc7cfa4b6adfbac92cd440e00
Lin Ruiyi <98309795+2380558221@users.noreply.github.com>
2022-04-30 18:05:55 +0800

    Update save_load.h

--- save_load.h
+++ save_load.h
@@ -3,8 +3,8 @@
 #define saveLoad_h
 #include "player.h"
 
-void save_game(Player* current_player, Player p1, Player p2, Char ** gameBoard);
+void save_game(Player* current_player, Player p1, Player p2, char ** gameBoard);
 
-void load_game(Player *& current_player, Player &p1, Player &p2, Char **& gameBoard);
+void load_game(Player *& current_player, Player &p1, Player &p2, char **& gameBoard);
 
 #endif
