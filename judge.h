1ada86af95aefbceb1160d1c40a764199751cd0f
Xiang Chen <cxiang@connect.hku.hk>
2022-05-01 21:23:54 +0800

    Updates judge headers

--- judge.h
+++ judge.h
@@ -2,9 +2,11 @@
 #ifndef judge_h
 #define judge_h
 #include "judge_helper.h"
-//Function: For judgeing if their is five in a row
-//Input: x,y: the coordinates of where the go was placed
+
+//Function: For judging if there is five in a row
+//Input: x,y: the coordinates of where the go was placed.   gameBoard: current game board.
+//        w: width of board.    h: height of board
 //Outputs: true or false for whether win or not
-bool judge(int x, int y);
+bool judge(int x, int y, char ** gameBoard, int w, int h);
 
 #endif
--- judge_helper.h
+++ judge_helper.h
@@ -2,19 +2,24 @@
 #ifndef judgeHelper_h
 #define judgeHelper_h
 
-//Function: Judgeing if their is five in a row in the horizontal direction
-//Input: x,y: the coordinate of the newly placed go
-//Output: true of false for whether their is five in a row in the horizontal direction
-bool judge_horizontal(int x, int y);
+//Function: Judging if there is five in a row in the horizontal direction
+//Input: x,y: the coordinate of the newly placed go.  boardWidth: the width of the board, gameBoard: game board
+//Output: true of false for whether there is five in a row in the horizontal direction
+bool judge_horizontal(int x, int y, char ** gameBoard, int boardWidth);
 
-//Function: Judgeing if their is five in a row in the vertical direction
-//Input: x,y: the coordinate of the newly placed go
-//Output: true of false for whether their is five in a row in the vertical direction
-bool judge_vertical(int x, int y);
+//Function: Judging if there is five in a row in the vertical direction
+//Input: x,y: the coordinate of the newly placed go. boardWidth: the Height of the board, gameBoard: game board
+//Output: true of false for whether there is five in a row in the vertical direction
+bool judge_vertical(int x, int y, char ** gameBoard, int boardHeight);
 
-//Function: Judgeing if their is five in a row in the diagonal directions
-//Input: x,y: the coordinate of the newly placed go
-//Output: true of false for whether their is five in a row in the diagonal directions
-bool judge_diagonal(int x, int y);
+//Function: Judging if there is five in a row in the diagonal directions left top to right down (\)
+//Input: x,y: the coordinate of the newly placed go. boardWidth,boardHeight: the width/height of the board, gameBoard: game board
+//Output: true of false for whether there is five in a row in the diagonal directions
+bool judge_leftTop_to_rightDown_diagonal(int x, int y, char ** gameBoard, int boardHeight, int boardWidth);
+
+//Function: Judging if there is five in a row in the diagonal directions left down to right top (/)
+//Input: x,y: the coordinate of the newly placed go. boardWidth,boardHeight: the width/height of the board, gameBoard: game board
+//Output: true of false for whether there is five in a row in the diagonal directions
+bool judge_leftDown_to_rightTop_diagonal(int x, int y, char ** gameBoard, int boardHeight, int boardWidth);
 
 #endif
